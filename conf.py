0be208fe442dd9f0865ba5e9de8a3449ad567f19
astreet <github2@phox.org>
2012-03-11 22:26:06 -0700

    conf

--- conf.py
+++ conf.py
@@ -1,10 +1,8 @@
 import os
 
 class Config(object):
-    DEBUG = True
+    DEBUG = False
     TESTING = False
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-    FBAPI_SCOPE = ['user_likes', 'user_photos',
-                   'user_photo_video_tags']
     FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
     CACHE_TYPE = 'memcached'
