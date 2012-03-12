15783b9b7f809a3aa26057046a85515f1d51522a
astreet <github2@phox.org>
2012-03-12 00:04:13 -0700

    Heroku issue fixed, going back to correct memcache conf :p

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ class Config(object):
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
     FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
     CACHE_TYPE = 'memcached'
-    CACHE_MEMCACHED_SERVER = memcache.Client(
+    CACHE_MEMCACHED_SERVER = pylibmc.Client(
         servers=[os.environ.get('MEMCACHE_SERVERS')],
         username=os.environ.get('MEMCACHE_USERNAME'),
         password=os.environ.get('MEMCACHE_PASSWORD'),
